40c1c8ef451a420c800d39b12856bdafc6ef1e27
Vincent Lussenburg <vlussenburg@xebia.com>
2016-03-02 16:02:49 -0800

    Restructured and removed post-verification because checksum can change when replacing placeholders

--- src/main/resources/planning/validation.py
+++ src/main/resources/planning/validation.py
@@ -6,7 +6,7 @@
 
 from java.util import HashSet
 
-#Grab all deployables
+# Grab all deployables
 def unique_deployables():
     result = HashSet()
     for delta in deltas.deltas:
@@ -15,7 +15,7 @@ def unique_deployables():
             result.add(deployed.deployable)
     return result
         
-#Create a before validation step for each deployable artifact
+# Create a before validation step for each deployable artifact
 for deployable in unique_deployables():
     context.addStep(steps.jython(
         description="Validate checksum of %s" % deployable.name,
@@ -23,22 +23,3 @@ for deployable in unique_deployables():
         script="scripts/validation/before_validation.py",
         jython_context={'deployable': deployable})
     )
-
-#Cycle through every delta and its corresponding deployables and containers to create an os-script step for after validation
-#Manually pass values into the freemarker context
-#Every container MUST have a "home" property
-#Every deployable must install through the container's home property
-for delta in deltas.deltas:
-    if delta.operation == "CREATE" or delta.operation == "MODIFY":
-        deployed = delta.deployedOrPrevious
-        container = deployed.container
-        deployable = deployed.deployable
-        checksum = deployable["checksum"]
-        
-        context.addStep(steps.os_script(
-            description="Validate checksum of %s for %s on %s" % (deployable.name, container.name, container.host.name),
-            order=0,
-            script="scripts/validation/after_validation",
-            freemarker_context={'checksum': checksum, 'deployedFileName': deployed.file.name, 'containerHome': container.home},
-            target_host=container.host)
-        )
--- src/main/resources/scripts/validation/before_validation.py
+++ src/main/resources/scripts/validation/before_validation.py
@@ -4,22 +4,26 @@
 # FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
 #
 
-#Grab the SHA1 hash of any filepath
-#Read in as binary mode to match XLD
+# Grab the SHA1 hash of filepath
 def sha1OfFile(filepath):
     import hashlib
+    # Read in as binary mode to match XLD
     with open(filepath, 'rb') as f:
         return hashlib.sha1(f.read()).hexdigest()
 
-#Grab the checksum and filepath of a deployable artifact
-#Calculate and compare values, throw an exception in order to stop the deployment
-checksum = deployable["checksum"]
-dep_file = deployable.file
-dep_file_location = dep_file.path
-print 'Deployable Checksum: ' + str(checksum)
-print 'Calculated Checksum: ' + str(sha1OfFile(dep_file_location))
-if (str(checksum) == str(sha1OfFile(dep_file_location))):
+# Grab the checksum and filepath of a deployable artifact
+checksum = str(deployable["checksum"])
+deployable_file = deployable.file
+deployable_file_location = deployable_file.path
+calculated_checksum = str(sha1OfFile(deployable_file_location)
+
+print 'Deployable Checksum: %s' % checksum
+# Calculate checksum on the filesystem
+print 'Calculated Checksum: %s' % calculated_checksum
+
+if (checksum == calculated_checksum):
     print 'Checksum match!'
 else:
     print 'Checksum mismatch!'
+    # Throw an exception in order to stop the deployment
     raise ValueError('Checksum mismatch!')
